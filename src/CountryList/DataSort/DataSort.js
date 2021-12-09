63627d80191882d9cab2ff5963debbcf991fe118
KA75AU <Patrick.Joseph.Sanchez@ing.com>
2021-12-09 16:33:30 +0800

    improved mobile view

--- src/CountryList/DataSort/DataSort.js
+++ src/CountryList/DataSort/DataSort.js
@@ -43,7 +43,6 @@ export class DataSort extends LitElement {
         box-shadow: 0 4px 10px 0 rgba(0, 0, 0, 0.2),
           0 4px 20px 0 rgba(0, 0, 0, 0.19);
         text-align: -webkit-center;
-        -webkit-appearance: textfield;
       }
 
       #orderButton {
